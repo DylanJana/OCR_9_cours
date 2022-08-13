f13387f62d7a09f630eccd0cd3bd04064cbf6731
Dylan Durand <dylan.durand88@outlook.fr>
2022-08-13 12:29:40 +0200

    API weatherstack

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,11 +1,11 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = '54aca7d8d36d865166da9c780f631a5d'
 
 const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
 .then(res => res.json())
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
     .then(res => res.json())
     .catch(err => console.log("Oh no", err))
 
